15fad0c1b775faa665416b8aca343577b777972c
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2020-07-23 07:17:38 +0100

    Added 'the field {string} contains {string}' and tested Added 'the field {string} does not contain {string}' and tested

---
+++ cypress/support/step_definitions/contains-or-does-not-contain-value.js
@@ -0,0 +1,17 @@
+
+
+Given('the field {string} contains {string}', (elementSelector, assertionValue) => {
+    cy
+        .get(elementSelector)
+        .should((elementSelector) => {
+            expect(elementSelector).to.have.value(assertionValue)
+        })
+});
+
+Given('the field {string} does not contain {string}', (elementSelector, assertionValue) => {
+    cy
+        .get(elementSelector)
+        .should((elementSelector) => {
+            expect(elementSelector).not.have.value(assertionValue)
+        })
+});
